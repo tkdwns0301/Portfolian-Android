0eeb8aa45dd0e18bb2989798a8f7f50dc1aa9048
hand <tkdwns0301@gmail.com>
2021-10-10 23:08:44 +0900

    [Add] New Project
    2021_10_10

---
+++ Portfolian/app/src/main/java/com/example/portfolian/View/LogInActivity.kt
@@ -0,0 +1,14 @@
+package com.example.portfolian.View
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.portfolian.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+}
