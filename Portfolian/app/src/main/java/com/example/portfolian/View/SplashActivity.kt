011202ff8a58c8d5dc2ae39a541f12b6ac4db30f
hand <tkdwns0301@gmail.com>
2021-10-11 22:07:21 +0900

    [Add] Splash View
    2021_10_11

---
+++ Portfolian/app/src/main/java/com/example/portfolian/View/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.portfolian.View
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.portfolian.R
+
+class SplashActivity: AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+
+    }
+}
