f1323711640ffea4342e5aad2b54d953fca97680
hand <tkdwns0301@gmail.com>
2021-10-20 19:26:31 +0900

    Feat: 메인액티비티 실행
    2021_10_20

--- Portfolian/app/src/main/java/com/example/portfolian/view/login/NicknameFragment.kt
+++ Portfolian/app/src/main/java/com/example/portfolian/view/login/NicknameFragment.kt
@@ -5,14 +5,22 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.MediaController
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.portfolian.R
 
 class NicknameFragment : Fragment(R.layout.fragment_nickname) {
+    private lateinit var navController: NavController
+    private lateinit var btn_Next: ImageButton
+    private lateinit var et_Nickname: EditText
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
         init()
     }
 
@@ -25,14 +33,14 @@ class NicknameFragment : Fragment(R.layout.fragment_nickname) {
     }
 
     private fun initNextButton() {
-        val btn_Next = requireView().findViewById<ImageButton>(R.id.img_btn_Next)
+        btn_Next = requireView().findViewById(R.id.img_btn_Next)
         btn_Next.setOnClickListener {
             initNickname()
         }
     }
 
     private fun initNickname() {
-        val et_Nickname = requireView().findViewById<EditText>(R.id.et_Nickname)
+        et_Nickname = requireView().findViewById(R.id.et_Nickname)
 
         if(et_Nickname.text.isNullOrBlank()) {
             Toast.makeText(activity, "닉네임을 입력해주세요.", Toast.LENGTH_SHORT).show()
@@ -40,7 +48,8 @@ class NicknameFragment : Fragment(R.layout.fragment_nickname) {
         } else {
             //TODO 정상적으로 메인페이지 넘어가기
             Log.d("NicknameFragment:: ", "SUCCESS")
-
+            navController.navigate(R.id.action_nicknameFragment_to_mainActivity)
+            activity?.finish()
         }
     }
 }
