a5bfe1ce4c5f746a58492f92decef06717854287
hand <tkdwns0301@gmail.com>
2021-10-20 19:16:11 +0900

    Feat: 로그인 기능
    2021_10_20

--- Portfolian/app/src/main/java/com/example/portfolian/SplashActivity.kt
+++ Portfolian/app/src/main/java/com/example/portfolian/SplashActivity.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.portfolian.login.LogInActivity
+import com.example.portfolian.view.login.LogInActivity
 
 class SplashActivity: AppCompatActivity() {
     private val SPLASH_TIME_OUT: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
             intent = Intent(this, LogInActivity::class.java)
