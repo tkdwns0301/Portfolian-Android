0744e96d8b4d55eb1a298d3ec268fede2ca16f68
hand <tkdwns0301@gmail.com>
2021-10-29 00:32:08 +0900

    Feat: 필터 레이아웃 작성
    2021_10_29

--- Portfolian/app/src/main/java/com/example/portfolian/view/main/home/HomeFragment.kt
+++ Portfolian/app/src/main/java/com/example/portfolian/view/main/home/HomeFragment.kt
@@ -3,61 +3,66 @@ package com.example.portfolian.view.main.home
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.portfolian.R
 import com.example.portfolian.adapter.ProjectAdapter
 import com.example.portfolian.data.Project
-import com.example.portfolian.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private var mBinding: FragmentHomeBinding? = null
-    private val binding get() = mBinding!!
     private lateinit var rv_Project: RecyclerView
     private lateinit var adapter: ProjectAdapter
     private lateinit var sl_Swipe: SwipeRefreshLayout
-    private lateinit var testProject: Project
+    private lateinit var dl_Main: DrawerLayout
+    private lateinit var cl_Drawer: LinearLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var btn_AllClick: Button
+    private lateinit var btn_Close: ImageButton
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
-        init()
-        return binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initSwipeRefreshLayout(view)
+        initRecyclerView(view)
+        initToolbar(view)
+        initDrawer(view)
     }
 
-    private fun init() {
-        initRecyclerView()
-        initSwipeRefreshLayout()
-        initToolbar()
-    }
 
-    private fun initSwipeRefreshLayout() {
-        sl_Swipe = binding.slSwipe
+    private fun initSwipeRefreshLayout(view: View) {
+        sl_Swipe = view.findViewById(R.id.sl_Swipe)
         sl_Swipe.setOnRefreshListener {
             refresh()
         }
     }
-    private fun initRecyclerView() {
-        rv_Project = binding.rvProject
+    private fun initRecyclerView(view: View) {
+        rv_Project = view.findViewById(R.id.rv_Project)
         val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         rv_Project.layoutManager = layoutManager
         readProject()
     }
 
-    private fun initToolbar() {
-        val toolbar = binding.toolbarHome
+    private fun initToolbar(view: View) {
+        toolbar = view.findViewById(R.id.toolbar_home)
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.toolbar_Filter -> {
                     Log.d("HomeFragment: ", "Filter Button Click")
-                    findNavController().navigate(R.id.action_homeFragment_to_homeFilterFragment)
+                    dl_Main = view.findViewById(R.id.dl_Main)
+                    cl_Drawer = requireView().findViewById(R.id.cl_Drawer)
+                    dl_Main.openDrawer(cl_Drawer)
+
+
                     true
                 }
                 R.id.toolbar_Alert -> {
@@ -68,6 +73,20 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                     super.onOptionsItemSelected(it)
                 }
             }
+        }
+    }
+
+    private fun initDrawer(view: View) {
+        btn_AllClick = view.findViewById(R.id.btn_AllClick)
+
+        btn_AllClick.setOnClickListener {
+            Log.d("click: ", "success")
+        }
+
+        btn_Close = view.findViewById(R.id.img_btn_Close)
+        btn_Close.setOnClickListener {
+            Log.d("Close", "success")
+            dl_Main.closeDrawers()
         }
     }
 
@@ -90,4 +109,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         readProject()
         sl_Swipe.isRefreshing = false
     }
+
+
+
 }
