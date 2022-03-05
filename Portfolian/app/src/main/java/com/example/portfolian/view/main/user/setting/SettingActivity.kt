c500125380a5540cb38119c480b0fdf91e89ae3c
hand <tkdwns0301@gmail.com>
2022-03-05 16:43:19 +0900

    Feat: 로그아웃 구현
    2022_03_05

--- Portfolian/app/src/main/java/com/example/portfolian/view/main/user/setting/SettingActivity.kt
+++ Portfolian/app/src/main/java/com/example/portfolian/view/main/user/setting/SettingActivity.kt
@@ -3,14 +3,17 @@ package com.example.portfolian.view.main.user.setting
 import android.content.Intent
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.portfolian.R
+import com.example.portfolian.view.CustomDialog
 
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
     private lateinit var notify: LinearLayout
+    private lateinit var logout: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,7 @@ class SettingActivity : AppCompatActivity() {
     private fun init() {
         initToolbar()
         initNotifySetting()
+        initLogout()
     }
 
     private fun initToolbar() {
@@ -38,6 +42,15 @@ class SettingActivity : AppCompatActivity() {
         notify.setOnClickListener {
             val intent = Intent(this, NotifyActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun initLogout() {
+        logout = findViewById(R.id.tv_Logout)
+
+        logout.setOnClickListener {
+            val customDialog = CustomDialog(this)
+            customDialog.showDialog()
         }
     }
 }
